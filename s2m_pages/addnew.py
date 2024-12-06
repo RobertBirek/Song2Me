bb3691ffdfa285974ed8e53d0ea6cda9297a0f2f
Robert Birek <robert.birek@op.pl>
2024-12-06 15:00:01 +0100

    v3.3

--- s2m_pages/addnew.py
+++ s2m_pages/addnew.py
@@ -84,7 +84,7 @@ def show_page():
                             #         "preferredquality": "128",
                             #     }
                             # ],
-                            # "geo_bypass": True, # Pomijanie ograniczeń regionalnych
+                            "geo_bypass": True, # Pomijanie ograniczeń regionalnych
                             # "headers": {
                             #     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
                             #     "Accept-Language": "en-US,en;q=0.9",
