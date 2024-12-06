26ed16a12e2b510bc8722e431b73d7f04ea34222
Robert Birek <robert.birek@op.pl>
2024-12-06 11:45:38 +0100

    v3.3

--- s2m_pages/addnew.py
+++ s2m_pages/addnew.py
@@ -79,12 +79,15 @@ def show_page():
                                 {
                                     "key": "FFmpegExtractAudio",
                                     "preferredcodec": "mp3",
-                                    "http_headers": {
-                                        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
-                                        "Accept-Language": "en-US,en;q=0.9"},
                                     "preferredquality": "192",
                                 }
                             ],
+                            "geo_bypass": True, # Pomijanie ograniczeń regionalnych
+                            "headers": {
+                                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
+                                "Accept-Language": "en-US,en;q=0.9",
+                            },
+
                         }
                         with YoutubeDL(ydl_opts) as ydl:
                             ydl.download([youtube_url])
