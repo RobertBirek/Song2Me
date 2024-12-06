24e384e570485d7f1fc22d43d9b1ae68a7e1678f
Robert Birek <robert.birek@op.pl>
2024-12-06 10:53:32 +0100

    v3.3

--- s2m_pages/addnew.py
+++ s2m_pages/addnew.py
@@ -81,7 +81,7 @@ def show_page():
                                     "preferredcodec": "mp3",
                                     "http_headers": {
                                         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
-                                        "Accept-Language": "en-US,en;q=0.9",
+                                        "Accept-Language": "en-US,en;q=0.9"},
                                     "preferredquality": "192",
                                 }
                             ],
